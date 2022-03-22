e272b434492bca7cc5e1c191060c860bbf5589f8
Pop-Os <ivan@pop-os.localdomain>
2022-03-22 17:25:11 -0300

    Requisito 1

--- src/jobs.py
+++ src/jobs.py
@@ -1,6 +1,9 @@
+import csv
 from functools import lru_cache
 
 
 @lru_cache
 def read(path):
-    return []
+    with open(path, encoding="utf8") as jobs_csv:
+        jobs = csv.DictReader(jobs_csv, delimiter=",")
+        return [row for row in jobs]
