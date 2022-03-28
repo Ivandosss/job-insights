e9132bdc72faa2022c325104531d98982a44347e
Pop-Os <ivan@pop-os.localdomain>
2022-03-28 14:53:05 -0300

    Requisito 9 e 10

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,106 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+import pytest
 
 
-def test_sort_by_criteria():
-    pass
+@pytest.fixture
+def jobs_sort_by_criteria():
+
+    return [
+        {
+            "max_salary": "10000",
+            "min_salary": "5000",
+            "date_posted": "2021-03-08",
+        },
+        {
+            "max_salary": "8000",
+            "min_salary": "4000",
+            "date_posted": "2021-03-20",
+        },
+        {
+            "max_salary": "2000",
+            "min_salary": "1000",
+            "date_posted": "2021-03-06",
+        },
+        {
+            "max_salary": "5000",
+            "min_salary": "2000",
+            "date_posted": "2021-03-16",
+        },
+    ]
+
+
+sort_mock = {
+    "date_posted": [
+        {
+            "max_salary": "8000",
+            "min_salary": "4000",
+            "date_posted": "2021-03-20",
+        },
+        {
+            "max_salary": "5000",
+            "min_salary": "2000",
+            "date_posted": "2021-03-16",
+        },
+        {
+            "max_salary": "10000",
+            "min_salary": "5000",
+            "date_posted": "2021-03-08",
+        },
+        {
+            "max_salary": "2000",
+            "min_salary": "1000",
+            "date_posted": "2021-03-06",
+        },
+    ],
+    "min_salary": [
+        {
+            "max_salary": "2000",
+            "min_salary": "1000",
+            "date_posted": "2021-03-06",
+        },
+        {
+            "max_salary": "5000",
+            "min_salary": "2000",
+            "date_posted": "2021-03-16",
+        },
+        {
+            "max_salary": "8000",
+            "min_salary": "4000",
+            "date_posted": "2021-03-20",
+        },
+        {
+            "max_salary": "10000",
+            "min_salary": "5000",
+            "date_posted": "2021-03-08",
+        },
+    ],
+    "max_salary": [
+        {
+            "max_salary": "10000",
+            "min_salary": "5000",
+            "date_posted": "2021-03-08",
+        },
+        {
+            "max_salary": "8000",
+            "min_salary": "4000",
+            "date_posted": "2021-03-20",
+        },
+        {
+            "max_salary": "5000",
+            "min_salary": "2000",
+            "date_posted": "2021-03-16",
+        },
+        {
+            "max_salary": "2000",
+            "min_salary": "1000",
+            "date_posted": "2021-03-06",
+        },
+    ],
+}
+
+
+def test_sort_by_criteria(jobs):
+    criterias = ["min_salary", "max_salary", "date_posted"]
+    for criteria in criterias:
+        sort_by(jobs, criteria)
+        assert jobs == sort_mock[criteria]
